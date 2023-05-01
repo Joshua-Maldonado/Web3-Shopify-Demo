5d7459e028ab8a78c35308d7a47d07df69d44e19
Josh Maldonado <joshm@topdrawermerch.com>
2023-05-01 12:42:48 -0600

    font-update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,29 @@ import NextHead from 'next/head'
 import { WagmiConfig } from 'wagmi'
 import { client } from '../src/wagmi'
 import Page from './index'
+import localFont from 'next/font/local'
 import '../src/style.css'
 
 import Script from 'next/script'
+
+
+const PtRootUi = localFont({
+  src: [
+    {
+      path: '../public/fonts/pt-root-ui.woff2',
+      weight: '400',
+    },
+    {
+      path: '../public/fonts/pt-root-ui_medium.woff2',
+      weight: '500',
+    },
+    {
+      path: '../public/fonts/pt-root-ui_bold.woff2',
+      weight: '700',
+    },
+  ],
+});
+
 
 function App() {
   
@@ -19,7 +39,20 @@ function App() {
           <title>Top Drawer Merch Web3 Demo</title>
           <link rel="shortcut icon" href="favicon.png" />
         </NextHead>
-
+        <style jsx global>{`
+        :root {
+          /* ... */
+          --pt-root-ui: ${PtRootUi.style.fontFamily};
+        }
+        p,label{
+          font-family: ${PtRootUi.style.fontFamily} !important;
+          font-weight: 400;
+        }
+        h1,h2,h3,h4,h5 {
+          font-family: ${PtRootUi.style.fontFamily} !important;
+          font-weight: 500;
+        }
+      `}</style>
         <Script
               id="googlemaps"
               type="text/javascript"
